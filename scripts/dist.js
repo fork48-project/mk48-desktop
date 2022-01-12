fe2041440f393bec8bf4882ec484d99e4216f495
thatretrodev <thatretrodev@outlook.com>
2022-01-12 16:09:31 -0500

    Add support for packaging the app via electron-packager

---
+++ scripts/dist.js
@@ -0,0 +1,31 @@
+const { execSync } = require('child_process');
+
+function dist(platform, arch) {
+	console.log(`Packaging app with platform: ${platform} and arch: ${arch}`);
+
+	try {
+		const zipCommand = platform == "linux" ? `tar -czf dist/mk48-${platform}-${arch}.tar.gz dist/mk48-${platform}-${arch}/` : `zip -r dist/mk48-${platform}-${arch}.zip dist/mk48-${platform}-${arch}/`;
+		execSync(`npx electron-packager . mk48 --asar --out=dist --platform=${platform} --arch=${arch} --ignore=\"(src*|tsconfig.json|.gitignore|scripts*|dist*)\";${zipCommand};rm -rf dist/mk48-${platform}-${arch}/`);
+	} catch (e) {}
+}
+
+function dist_darwin() {
+	dist("darwin", "x64");
+	dist("darwin", "arm64");
+}
+
+function dist_linux() {
+	dist("linux", "x64");
+	dist("linux", "armv7l");
+	dist("linux", "arm64");
+}
+
+function dist_win32() {
+	dist("win32", "ia32");
+	dist("win32", "x64");
+	dist("win32", "arm64");
+}
+
+dist_linux();
+dist_win32();
+dist_darwin();
