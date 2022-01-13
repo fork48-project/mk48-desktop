2ff0ccc86719807bfd194d65edc8d480dfaf586f
thatretrodev <thatretrodev@outlook.com>
2022-01-12 20:35:24 -0500

    Edit index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ const {app, BrowserWindow} = require('electron');
 const path = require('path');
 const DiscordRPC = require('discord-rpc');
 
-function getSettings(name: string) : boolean {
+function getSettings(name: string) : string {
 	if (typeof process.env["MK48_" + name.toUpperCase()] !== "undefined") {
-		console.log(`${"MK48_" + name.toUpperCase()} does exist ${process.env["MK48_" + name.toUpperCase()] == "enabled" ? "and is enabled": "but is not enabled"}`);
+		//console.log(`${"MK48_" + name.toUpperCase()} does exist ${process.env["MK48_" + name.toUpperCase()] == "enabled" ? "and is enabled": "but is not enabled"}`);
 
-		return process.env["MK48_" + name.toUpperCase()] == "enabled";
+		return process.env["MK48_" + name.toUpperCase()];
 	}
-	console.log(`${"MK48_" + name.toUpperCase()} does not exist.`);
-	return false;
+	//console.log(`${"MK48_" + name.toUpperCase()} does not exist.`);
+	return "disabled";
 }
 
 var mainWindow;
@@ -21,18 +21,18 @@ function createWindow() {
 		width: 1280,
 		height: 720,
 		webPreferences: {
-			preload: path.join(__dirname, 'preload.js')
+			preload: path.join(__dirname, "preload.js")
 		}
 	});
 
 	// and load the index.html of the app.
-	mainWindow.loadURL('https://mk48.io');
+	mainWindow.loadURL("https://mk48.io");
 	mainWindow.setMenu(null);
 	
 	//mainWindow.loadFile('index.html');
 
 	// Open the DevTools.
-	if (getSettings("debugmenu")) {
+	if (getSettings("debugmenu") == "enabled") {
 		mainWindow.webContents.openDevTools();
 	}
 }
@@ -43,7 +43,7 @@ function createWindow() {
 app.whenReady().then(() => {
 	createWindow();
 
-	app.on('activate', function () {
+	app.on("activate", function () {
 		// On macOS it's common to re-create a window in the app when the
 		// dock icon is clicked and there are no other windows open.
 		if (BrowserWindow.getAllWindows().length === 0) {
@@ -55,19 +55,19 @@ app.whenReady().then(() => {
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-	if (process.platform !== 'darwin') {
+app.on("window-all-closed", function () {
+	if (process.platform !== "darwin") {
 		app.quit();
 	}
 });
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
-const clientId = '869542716961345556';
+const clientId = "869542716961345556";
 
 DiscordRPC.register(clientId);
 
-const rpc = new DiscordRPC.Client({ transport: 'ipc' });
+const rpc = new DiscordRPC.Client({ transport: "ipc" });
 const startTimestamp = new Date();
 
 async function setActivity() {
@@ -82,11 +82,11 @@ async function setActivity() {
 		state: `Name: "${playername}"`,
 		startTimestamp,
 		instance: false,
-		largeImageKey: 'mk48io3'
+		largeImageKey: "mk48io3"
 	});
 }
 
-rpc.on('ready', () => {
+rpc.on("ready", () => {
 	setActivity();
 
 	setInterval(() => {
